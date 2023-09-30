c535f7a7b5371476035a3384fa3e1fdf1f7ae15f
chichileishvili <79799309+chichileishvili@users.noreply.github.com>
2023-09-30 10:07:08 +0400

    Initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDK04xzB5PEPpRwtp13zw8tplwlf3UAQR8',
+
+  authDomain: 'dojo-b7414.firebaseapp.com',
+
+  projectId: 'dojo-b7414',
+
+  storageBucket: 'dojo-b7414.appspot.com',
+
+  messagingSenderId: '326087778786',
+
+  appId: '1:326087778786:web:c99a53da47dfe9f9ced9bb',
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth
+
+const timestamp = firebase.firestore.Timestamp
+
+export { projectAuth, projectFirestore, timestamp }
