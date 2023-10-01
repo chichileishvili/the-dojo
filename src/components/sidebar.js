7ea5d5a576949e2f9efb3173e616b5031b7d69e9
chichileishvili <79799309+chichileishvili@users.noreply.github.com>
2023-10-01 15:44:56 +0400

    add: user avatar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,14 +2,19 @@ import './sidebar.css'
 import Dashboard from '../assets/dashboard_icon.svg'
 import AddIcon from '../assets/add_icon.svg'
 import { NavLink } from 'react-router-dom'
+import Avatar from './avatar'
+import { useAuthContext } from '../hooks/useAuthContext'
 export default function Sidebar() {
+  const { user } = useAuthContext()
   return (
     <div className='sidebar'>
       <div className='sidebar-content'>
-        <div className='user'>
-          {'avatar and username'}
-          <p>hey user</p>
-        </div>
+        {user && (
+          <div className='user'>
+            <Avatar src={user.photoURL} />
+            <p>hey {user.displayName}</p>
+          </div>
+        )}
         <nav className='links'>
           <ul>
             <li>
