d2f8532b883fb7ac21db8239c3d777470ec5738e
chichileishvili <79799309+chichileishvili@users.noreply.github.com>
2023-10-02 14:07:14 +0400

    add: filter component

--- src/pages/project/projectSummary.js
+++ src/pages/project/projectSummary.js
@@ -1,11 +1,22 @@
 import Avatar from '../../components/avatar'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useNavigate } from 'react-router-dom'
 import './project.css'
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore('projects')
+  const { user } = useAuthContext()
+  const history = useNavigate()
+  const handleClick = (e) => {
+    deleteDocument(project.id)
+    history('/')
+  }
   return (
     <div>
       <div className='project-summary'>
         <h2 className='page-title'></h2>
+        <p> By: {project.createdBy.displayName}</p>
         <p className='due-date'>Project due date {project.dueDate.toDate().toDateString()}</p>
         <p className='details'>{project.details}</p>
         <h5>project is assigned to:</h5>
@@ -18,6 +29,12 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {user.uid == project.createdBy.id && (
+        <button onClick={handleClick} className='btn'>
+          {' '}
+          Mark As Completed
+        </button>
+      )}
     </div>
   )
 }
